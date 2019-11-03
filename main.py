5eb889869f0c9e6a560bf2e2453b98b2ae3a54d5
joebu23 <joe.royston@gmail.com>
2019-11-03 17:18:14 -0500

    updates

--- main.py
+++ main.py
@@ -6,35 +6,27 @@ from datetime import timedelta
 
 rangeData = []
 
-year = 2018
+year = 2017
 month = 10
-day = 15
+day = 23
 
 startDate = datetime.datetime(year, month, day)
 
-daysToGet = 190
+daysToGet = 70
 
 f = open("weatherData.csv","w+")
+weeksData = []
 
 for nDay in range(0,daysToGet):
-    # dateToGet = startDate + nDay
     dateToGet = startDate + timedelta(days=nDay)  
 
     data = helpers.getForecastData(dateToGet.year, str(dateToGet.month).zfill(2), str(dateToGet.day).zfill(2))
-    counter = 0
-    cityCounter = 0
-    dateCounter = 0
 
-    weeksData = []
-    # forecasts = []
-    # days = []
-    highs = []
-    lows = []
-
-    if data != '':
+    if len(data) > 1:
         dateData, forecastData, tempData, preciptData = textHelper.getLinesWithPertinentData(data)
 
-    if forecastData is not None:
+    try:
+        # if forecastData is not None:
         forecasts = textHelper.generateForecastData(forecastData)
         days = textHelper.generateDayparts(dateData, year)
         temps = textHelper.generateForecastData(tempData)
@@ -64,15 +56,11 @@ for nDay in range(0,daysToGet):
             else:
                 preciptAM = 9999
             
-            forecastDate = datetime.datetime(year, int(month), int(day))
-    
-            newObject = forecastDay.ForecastDay(date, high, low, preciptPM, preciptAM, forecast, forecastDate)
-            # print('date: ' + str(date) + ' high: ' + str(high) + ' low: ' + str(low) + ' forecast: ' + str(forecast) + ' fdate: ' + str(forecastDate))
-            # print('pm preceipt: ' + str(preciptPM) + ' am: ' + str(preciptAM))
-            # print((date - forecastDate).days)
-            # print(newObject)
+            newObject = forecastDay.ForecastDay(date, high, low, preciptPM, preciptAM, forecast, dateToGet)
             weeksData.append(newObject)
-
+    except:
+        newObject = forecastDay.ForecastDay(dateToGet, 999, 999, 999, 999, 'NA', datetime.datetime(1,1,1))
+        weeksData.append(newObject)
 
 for data in weeksData:
-    f.write(str(data.date) + ',' + str((data.date - data.forecastDate).days) + ',' + str(data.high) + ',' + str(data.low))
+    f.write(str(data.date) + ',' + str((data.date - data.forecastDate).days) + ',' + str(data.high) + ',' + str(data.low) + '\n')
