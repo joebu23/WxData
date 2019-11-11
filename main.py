57ea7f65dc91fffe53d102f2ea30f607f8ebaabe
joebu23 <joe.royston@gmail.com>
2019-11-10 20:11:51 -0500

    don't track those files either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import dataPuller
 # import helpers
 # this will get the acutal data once the url is figured out
-dataPuller.getData(2006, 11, 1, 55)
+dataPuller.getData(2006, 11, 1, 54)
 
 # helpers.getUrl(2013, 10, 15)
